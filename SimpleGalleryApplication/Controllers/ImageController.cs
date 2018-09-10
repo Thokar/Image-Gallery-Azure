ab30e7ff79cb6fe6502560b048922a411f7ac464
Thomas Karbe <thomas.karbe@googlemail.com>
2018-09-10 20:39:30 +0200

    .

--- SimpleGalleryApplication/Controllers/ImageController.cs
+++ SimpleGalleryApplication/Controllers/ImageController.cs
@@ -12,47 +12,47 @@ using SimpleGalleryApplication.Service;
 
 namespace SimpleGalleryApplication.Controllers
 {
-    public class ImageController : Controller
+  public class ImageController : Controller
+  {
+    private readonly IConfiguration _config;
+    private readonly IImageService imageService;
+    private string AzureConnectionString { get; }
+
+    public ImageController(IConfiguration config, IImageService imageService)
     {
-        private readonly IConfiguration _config;
-        private readonly IImageService imageService;
-        private string AzureConnectionString { get; }
-
-        public ImageController(IConfiguration config, IImageService imageService)
-        {
-            this._config = config;
-            this.AzureConnectionString = config["AzureStorageConnectionString"];
-            this.imageService = imageService;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Upload()
-        {
-            var model = new UploadImageModel()
-            {
-            };
-            return View();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> UploadNewImage(IFormFile file, string title, string tags)
-        {
-            var container = this.imageService.GetBlobContainer(AzureConnectionString, "images");
-            var content = ContentDispositionHeaderValue.Parse(file.ContentDisposition);
-            var fileName = content.FileName.ToString().Replace('"', ' ').Trim();
-
-            // get a reference to a block blob 
-            var blockBlock = container.GetBlockBlobReference(fileName);
-            await blockBlock.UploadFromStreamAsync(file.OpenReadStream());
-            await imageService.SetImage(title, tags, blockBlock.Uri);
-
-            return RedirectToAction("Index", "Gallery");
-
-            //return Ok();
-        }
+      this._config = config;
+      this.AzureConnectionString = config["AzureStorageConnectionString"];
+      this.imageService = imageService;
     }
+
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    public IActionResult Upload()
+    {
+      var model = new UploadImageModel()
+      {
+      };
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> UploadNewImage(IFormFile file, string title, string tags)
+    {
+      var container = this.imageService.GetBlobContainer(AzureConnectionString, "images");
+      var content = ContentDispositionHeaderValue.Parse(file.ContentDisposition);
+      var fileName = content.FileName.ToString().Replace('"', ' ').Trim();
+
+      // get a reference to a block blob 
+      var blockBlock = container.GetBlockBlobReference(fileName);
+      await blockBlock.UploadFromStreamAsync(file.OpenReadStream());
+      await imageService.SetImage(title, tags, blockBlock.Uri);
+
+      return RedirectToAction("Index", "Gallery");
+
+      //return Ok();
+    }
+  }
 }
